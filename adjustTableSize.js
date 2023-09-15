1164f504b5f707b082eadeecf560ea1b13f302e8
許權麒 <87688576+chuanchihsu1219@users.noreply.github.com>
2023-09-15 17:01:12 +0800

    Update adjustTableSize.js

--- adjustTableSize.js
+++ adjustTableSize.js
@@ -7,14 +7,15 @@ window.onresize = function() {
 };
 
 function adjustTableSize() {
+    const tableContainer = document.getElementById('table-container');
     const table = document.getElementById('timetable');
     const windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
     const tableWidth = table.offsetWidth;
 
-    if (tableWidth > windowWidth) {
-        const scaleValue = windowWidth / tableWidth;
+    if (tableWidth > tableContainer.offsetWidth) {
+        const scaleValue = tableContainer.offsetWidth / tableWidth;
         table.style.transform = `scale(${scaleValue})`;
     } else {
-        table.style.transform = 'scale(0.8)';
+        table.style.transform = 'scale(1)';
     }
 }
